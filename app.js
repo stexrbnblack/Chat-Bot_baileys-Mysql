1f864b731b07c96262bfd97f522d0c7a2e3c92bb
Steven Ribon Blanco <114124624+stexrbnblack@users.noreply.github.com>
2023-07-06 23:21:48 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -26,62 +26,250 @@ const MYSQL_DB_PORT = process.env.MYSQL_DB_PORT
  * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
  */
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+const flowCliHist = addKeyword('').addAnswer(['Por favor *Tomar una Foto a la Historia Clínica* antes mencionado'],
+    // saveJpgHiCli(media, resp),               // Guarda en la DB la imagen del Historia Clínica (Clinic Histpry)) 'jpgHiCli
+    null,
+    null,
+    // [flowExam]
+)
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+const flowImgOrd = addKeyword('').addAnswer(['Por favor *Tomar una Foto a la Autorización* antes mencionado'],
+    // saveJpgAut(media, resp),                  // Guarda en la DB la imagen de la Autorizacion (Photo Autory) 'jpgAut'    
+    null,
+    null,
+    [flowCliHist]
+)
+
+const flowExam = addKeyword(['',' ']).addAnswer('`Sr./Sra. {name}, tenemos para ti una forma más fácil de gestionar tu solicitud, \n `' +
+    'en el siguiente link: https://rmn.com.co/agendamiento/')
+    .addAnswer('De lo contrario selecciona la opción que deseas realizar ver opciones: ')
+    .addAnswer([
+        '*1. Radiología simple*',
+        '*2. Ecografías*',
+        '*3. Densitometrías*',
+        '*4. Tomografías*',
+        '*5. Resonancias*',
+        '*6. Mamografías*',
+        '*7. Electrocardiogramas*',
+        '*8. Monitoreo ambulatorio de presión arterial*',
+        '*9. Holter dinámico en 24 horas*',
+        '*10. Estudios bajo sedación*',
+        '*11. Biopsias*',
+        '*12. Pruebas de esfuerzos*',
+        '*13. Ecocardiogramas*',
+        '*14. Consulta por Cardiología*'
+    ],
+        // saveTypeExam(addKeyword, addAnswer),                 // Guarda en la DB el tipo de Examen (Exam Typr) 'typeExam' 
+        null,
+        null,
+        [flowImgOrd]
+    )
+
+const flowCell = addKeyword([' ', ''])
+    .addAnswer(
+        ['Ingrese el *Numero telefonico* del Paciente.'],
+        { capture: true },
+
+        async (ctx) => {
+            
+            let cell = ctx.body
+
+            console.log(`Estupendo *${namePatient}*! te dejo el resumen de tu formulario
+            \n- Nombre y apellidos: *${namePatient}*
+            \n- ID: *${idDoc}*
+            \n- Telefono: *${cell}*
+            \n- Correo: *${email}*`)
+        },
+        [flowExam]
+    )
+
+const flowCorreo = addKeyword([' ', ''])
+    .addAnswer(
+        ['Ingrese el *Correo electronico* del Paciente al cual desea resivir la validacion de la cita.'],
+        { capture: true },
+
+        async (ctx) => {
+
+            let email = ctx.body
+
+            console.log(`Email: *${email}*`)
+        },
+        [flowCell]
+    )
+
+const flowFormPatient = addKeyword([' ', ''])
+    .addAnswer(
+        ['Ingrese el *Nombre completo* del Paciente.'],
+        { capture: true },
+
+        (ctx) => {
+            let namePatient
+
+            namePatient = ctx.body
+            console.log(`Nombre: *${namePatient}*`)
+        },
+        [flowCorreo]
+    )
+
+const flowFormOtro = addKeyword(['4', 'otro'])
+    .addAnswer(
+        ['Digite el Tipo de Documento de ciudadanía'],
+        { capture: true },
+
+        (ctx) => {
+
+            let TypeDocument = ctx.body
+            console.log('typo de doc: ', TypeDocument)
+            console.log(`CC: *${TypeDocument}*`)
+        }
+    ).addAnswer(
+        ['Digite el Documento anteriormente mencionado'],
+        { capture: true },
+
+        async (ctx) => {
+
+            let idDoc = ctx.body
+            console.log('NUMERO DE CC: ', idDoc)
+            console.log(`CC: *${idDoc}*`)
+        },
+        [flowFormPatient]
+    )
+
+const flowFormTI = addKeyword(['3', 'ti'])
+    .addAnswer(
+        ['Digite su Targeta de Identidad'],
+        { capture: true },
+
+        async (ctx) => {
+
+            let idDoc = ctx.body
+            console.log('NUMERO DE TI: ', idDoc)
+            console.log(`TI: *${idDoc}*`)
+        },
+        [flowFormPatient]
+    )
+
+const flowFormCE = addKeyword(['2', 'ce'])
+    .addAnswer(
+        ['Digite su Cédula de Extrangeria'],
+        { capture: true },
+
+        async (ctx) => {
+
+            let idDoc = ctx.body
+            console.log('NUMERO DE CE: ', idDoc)
+            console.log(`CE: *${idDoc}*`)
+        },
+        [flowFormPatient]
+    )
+
+const flowFormCC = addKeyword(['1', 'cc'])
+    .addAnswer(
+        ['Digite su Cédula de ciudadanía'],
+        { capture: true },
+
+        async (ctx) => {
+            let idDoc = ctx.body
+            console.log('NUMERO DE CC: ', idDoc)
+            // console.log(`CC: *${idDoc}*`)
+        },
+        [flowFormPatient]
+    )
+
+const flowAcep2 = addKeyword(['1', 'si', 'acepto']).addAnswer(
     [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
+        'Para el Agendamiento de Citas digita. ¿Cuál es el tipo de documento de identificación del usuario? \n',
+        '1. CC (Cédula de ciudadanía),\n *digite 1*',
+        '2. CE (Cédula de extranjería),\n *digite 2*',
+        '3. TI (Tarjeta de idUserentidUserad),\n *digite 3*',
+        '4. Otro, *digita 4*'
+    ],
+    { capture: true },
+
+    async (ctx) => {
+        let TypeDocument = ctx.body
+
+        if (TypeDocument == '1') {
+            TypeDocument = 'CC'
+        } else if (TypeDocument == '2') {
+            TypeDocument = 'CE'
+        }
+        else if (TypeDocument == '3') {
+            TypeDocument = 'TI'
+        }
+        else if (TypeDocument == '4') { TypeDocument = 'TI' }
+
+        // await flowDynamic(`Encantado tipo de documento *${TypeDocument}*, ctx: ${ctx.from}`)
+    },
+    [flowFormCC, flowFormCE, flowFormTI, flowFormOtro]
+    //  saveTypeDocument(addKeyword, addAnswer)           // Guarda en la DB el tipo de documento (Document Type) 'typeDocument' : ,
+)
+
+const flowResult = addKeyword(['2', 'descargar', 'resultados']).addAnswer(
+    [
+        'Ingrese al siguiente link para descargar sus resultados: ' +
+        '*https://rmn.actualpacs.com/patientportal/',
     ],
     null,
     null,
-    [flowSecundario]
 )
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+const flowAddCit = addKeyword(['1', 'si', 'cita']).addAnswer(
     [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
+        'Para continuar con nuestra solicitud debes aceptar la política de administración de datos personales del ' +
+        '*CENTRO DE RESONANCIA MAGNETICA DEL NORTE*. Que puede ser consultada en el siguiente link: https://rmn.com.co/politica-habeas-data/',
+
+        '*1. Si Acepto*',
+        '*2. No Acepto*'
     ],
     null,
     null,
-    [flowSecundario]
+    [flowAcep2]
 )
 
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+const flowAcep = addKeyword(['1', 'si', 'acepto']).addAnswer(
     [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
+        'Gracias por aceptar esta comunicación.',
+        'Para avanzar en este chat solo debes digitar el *número* de la opción que necesitas ' +
+        'Elige la opción que necesitas:',
+
+        '*1. Agendar Cita*',
+        '*2. Descarga de resultados*'
     ],
+    // (ctx) => {
+    // console.log('addKeyword: ', addKeyword, '\n' + 'addAnswer: ', addKeyword.addAnswer)
+    // console.log('ctx.body: ', ctx.body, '\n' + 'ctxbody: ', ctx)},
     null,
     null,
-    [flowSecundario]
+    [flowAddCit, flowResult]
 )
 
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+// const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+//     .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+//     .addAnswer(
+//         [
+//             'te comparto los siguientes links de interes sobre el proyecto',
+//             '👉 *doc* para ver la documentación',
+//             '👉 *gracias*  para ver la lista de videos',
+//             '👉 *discord* unirte al discord',
+//         ],
+//         null,
+//         null,
+//         [flowDocs, flowGracias, flowTuto, flowDiscord]
+//     )
+const flowPrincipal = addKeyword(['hola', 'buenas', 'noche', 'dias', 'informacion'])
+    .addAnswer('🙌 ¡Hola! Te damos la bienvenida al *Centro de Resonancia Magnética del Norte.* Para avanzar ' +
+        'en este chat solo debes digitar el *número* de la opción que necesitas.')
     .addAnswer(
         [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
+            'Al utilizar este medio, aceptas los términos y condiciones de WhatsApp. Si quieres ' +
+            'ampliar la información, ingresa aquí: https://www.whatsapp.com/legal',
+            '👉 *1. Si Acepto*',
+            '👉 *2. No Acepto*'
         ],
         null,
         null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
+        [flowAcep]
     )
 
 const main = async () => {
